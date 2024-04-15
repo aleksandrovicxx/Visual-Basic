1af3633650b3785309efe28a45a3e8ab09417caa
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-15 16:40:56 +0200

    comm

---
+++ V2/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim prviBroj As Integer
+        Dim drugiBroj As Integer
+        Console.WriteLine("Unesite prvi broj: ")
+        prviBroj = Console.ReadLine
+        Console.WriteLine("Unesite drugi broj: ")
+        drugiBroj = Console.ReadLine
+        Console.WriteLine($"{prviBroj} + {drugiBroj} = {prviBroj + drugiBroj}")
+        Console.WriteLine($"{prviBroj} - {drugiBroj} = {prviBroj - drugiBroj}")
+        Console.WriteLine($"{prviBroj} * {drugiBroj} = {prviBroj * drugiBroj}")
+        Console.WriteLine($"{prviBroj} / {drugiBroj} = {prviBroj / drugiBroj}")
+
+
+
+    End Sub
+
+End Module

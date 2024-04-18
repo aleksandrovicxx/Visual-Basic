800026fea2afcb057708d747a1ece05cead4001f
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-18 09:09:02 +0200

    comm

---
+++ 53/Module1.vb
@@ -0,0 +1,17 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim broj As Integer = 1
+        Console.WriteLine("Brojevi od 0 do 10 bez 4")
+        While broj <= 10
+            If broj = 4 Then
+                broj = broj + 1
+                Continue While
+            End If
+            Console.Write($"{broj}")
+            broj = broj + 1
+        End While
+        Console.WriteLine()
+    End Sub
+
+End Module
---
+++ 53/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("53")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("53")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("07e0e453-9e48-4b48-9d2a-01afd97f00d7")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>

286077b659853a6711941b0138ceb1b3ea163bd5
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-18 09:37:39 +0200

    cpom

---
+++ 58-59/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Prikazi = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 87)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(314, 292)
+        Me.TextBox3.TabIndex = 12
+        '
+        'Prikazi
+        '
+        Me.Prikazi.Location = New System.Drawing.Point(12, 58)
+        Me.Prikazi.Name = "Prikazi"
+        Me.Prikazi.Size = New System.Drawing.Size(314, 23)
+        Me.Prikazi.TabIndex = 11
+        Me.Prikazi.Text = "Prikazi"
+        Me.Prikazi.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(226, 9)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 8
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 9)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 35)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(75, 17)
+        Me.CheckBox1.TabIndex = 13
+        Me.CheckBox1.Text = "Deljiv sa 5"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Prikazi)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Prikazi As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ 58-59/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Prikazi_Click(sender As Object, e As EventArgs) Handles Prikazi.Click
+        Dim prvi As Integer
+        Dim drugi As Integer
+
+        prvi = Val(TextBox1.Text)
+        drugi = Val(TextBox2.Text)
+
+        For i As Integer = prvi To drugi
+            If CheckBox1.Checked Then
+                If Not i Mod 5 = 0 Then
+                    Continue For
+                End If
+            End If
+            TextBox3.Text += $"{i}" & vbNewLine
+        Next
+
+
+    End Sub
+End Class

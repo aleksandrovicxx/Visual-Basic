31c4e1085a6672f26ffbe5657c62a2afcec13286
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-18 09:29:55 +0200

    comm

---
+++ 58z1/Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Proizvod = New System.Windows.Forms.RadioButton()
+        Me.Prikazi = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(23, 15)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(237, 15)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(23, 41)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(52, 17)
+        Me.RadioButton1.TabIndex = 3
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Suma"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Proizvod
+        '
+        Me.Proizvod.AutoSize = True
+        Me.Proizvod.Location = New System.Drawing.Point(237, 41)
+        Me.Proizvod.Name = "Proizvod"
+        Me.Proizvod.Size = New System.Drawing.Size(90, 17)
+        Me.Proizvod.TabIndex = 4
+        Me.Proizvod.TabStop = True
+        Me.Proizvod.Text = "RadioButton2"
+        Me.Proizvod.UseVisualStyleBackColor = True
+        '
+        'Prikazi
+        '
+        Me.Prikazi.Location = New System.Drawing.Point(23, 64)
+        Me.Prikazi.Name = "Prikazi"
+        Me.Prikazi.Size = New System.Drawing.Size(314, 23)
+        Me.Prikazi.TabIndex = 5
+        Me.Prikazi.Text = "Button1"
+        Me.Prikazi.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(23, 93)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(314, 292)
+        Me.TextBox3.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Prikazi)
+        Me.Controls.Add(Me.Proizvod)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents BindingSource1 As BindingSource
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Proizvod As RadioButton
+    Friend WithEvents Prikazi As Button
+    Friend WithEvents TextBox3 As TextBox
+End Class
---
+++ 58z1/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Prikazi.Click
+        Dim brojp As Integer
+        Dim brojd As Integer
+
+        If RadioButton1.Checked Then
+            brojp = TextBox1.Text
+            brojd = TextBox2.Text
+            TextBox3.Text = $"{brojp + brojd}"
+        End If
+        If Proizvod.Checked Then
+            brojp = TextBox1.Text
+            brojd = TextBox2.Text
+            TextBox3.Text = $"{brojp * brojd}"
+        End If
+    End Sub
+
+End Class

76185c15f742f56abccf632f119ab73ffd1bfa06
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-19 10:25:08 +0200

    comm

--- 59.3/Form1.vb
+++ 59.3/Form1.vb
@@ -5,7 +5,7 @@
 
         For i As Integer = 1 To 999999
             If i Mod 17 = 0 Then
-                TextBox2.Text += $"{i} "
+                TextBox2.Text += $"{i} {vbNewLine}"
                 counter += 1
                 If counter = n Then
                     Exit For

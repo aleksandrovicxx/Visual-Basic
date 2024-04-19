2597495599cf5c73641c00e94ff374f9c52eff8c
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-19 10:19:34 +0200

    end of task

---
+++ 59.3/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim n As Integer = Val(TextBox1.Text)
+        Dim counter As Integer = 0
+
+        For i As Integer = 1 To 999999
+            If i Mod 17 = 0 Then
+                TextBox2.Text += $"{i}  "
+                counter += 1
+                If counter = n Then
+                    Exit For
+                End If
+            End If
+        Next
+
+    End Sub
+End Class

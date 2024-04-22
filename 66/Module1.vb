6ed322db1c1194c356983f21501e23371d63ce79
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-22 09:08:45 +0200

    66 over

---
+++ 66/Module1.vb
@@ -0,0 +1,19 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim imeStudenata() As String = {"Marko", "Darko", "Jelena", "Milica", "Sanja"}
+        Dim counter = 0
+
+        For i As Integer = imeStudenata.Length - 1 To 0 Step -1
+            Console.WriteLine(imeStudenata(i))
+        Next
+        Console.WriteLine("-------------------------------")
+
+        For Each ime As String In imeStudenata
+            Console.WriteLine(imeStudenata(imeStudenata.Length - 1 - counter))
+            'Console.WriteLine(imeStudenata.Length - 1 - counter)
+            counter = counter + 1
+        Next
+    End Sub
+
+End Module

88f6950deae2cc77cd9ae74c329ae288855c5e73
aleksandrovicxx <aleksandrovicmilos99@gmail.com>
2024-04-22 09:24:48 +0200

    comm

--- 67/Module1.vb
+++ 67/Module1.vb
@@ -2,7 +2,6 @@
 
     Sub Main()
         Dim brojevi() As Integer = {7, 3, 1, 4, 2}
-        Dim pom As Integer
 
         For Each broj As Integer In brojevi
             Console.WriteLine(broj)
@@ -11,7 +10,7 @@
         For i As Integer = brojevi.Length - 1 To 0 Step -1
             For j = 0 To i - 1
                 If brojevi(j + 1) < brojevi(j) Then
-                    pom = brojevi(j)
+                    Dim pom As Integer = brojevi(j)
                     brojevi(j) = brojevi(j + 1)
                     brojevi(j + 1) = pom
                 End If
